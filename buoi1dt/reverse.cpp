7e4a97a3e0878566196bdcd088f3d840f5d90a80
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2025-09-23 16:32:56 +0700

    Solution exercise 2

---
+++ buoi1dt/reverse.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    string s; getline(cin, s);
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+
+    return 0;
+}
