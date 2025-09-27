57811a2b8813466f916af7ee9a596c3a92f9d200
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2025-09-27 15:16:39 +0700

    Solution problem perchk

---
+++ buoi2dt/perchk.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    long long n; cin >> n;
+
+    vector<int>a(n + 1);
+
+    for(long long i = 0; i < n; i++)
+    {
+        long long x; cin >> x;
+        a[x] = 1;
+    }
+
+    for(long long i = 1; i <= n; i++)
+    {
+        if(a[i] == 0)
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+
+    cout << "YES" << endl;
+
+    return 0;
+}
