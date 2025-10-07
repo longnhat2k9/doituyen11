2e635854ac4066d5e86c2219e69b7880ff00c958
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2025-10-07 15:12:06 +0700

    Fix Bugs For Problem 3

--- lis/may.cpp
+++ lis/may.cpp
@@ -15,39 +15,45 @@ int main()
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     long long n; cin >> n;
-    vector<pair<pair<long long, long long>, long long>>a(n);
-    for(long long i = 0; i < n; i++)
+    vector<pair<pair<long long, long long>, long long>>a(n + 1, {{0, 0}, 0});
+    for(long long i = 1; i <= n; i++)
     {
         cin >> a[i].fi >> a[i].sc >> a[i].th;
     }
-    sort(a.begin(), a.end(), cmp);
+    sort(a.begin() + 1, a.end(), cmp);
 
-    vector<long long>lis, f(n, -1);
-    long long mxl = -1;
+    vector<long long>lis(1, 0), f(n + 1, 0), val(1, 0);
+    long long mxl = 0;
 
-    for(long long i = 0; i < n; i++)
+    for(long long i = 1; i <= n; i++)
     {
         auto it = upper_bound(lis.begin(), lis.end(), a[i].fi);
 
         if(it == lis.end())
         {
             lis.push_back(a[i].sc);
-            f[i] = lis.size();
+            f[i] = f[mxl] + a[i].th;
+            val.push_back(f[i]);
             mxl = i;
         }
         else
         {
-            *it = min(*it, a[i].sc);
-            f[i] = it - lis.begin() + 1;
+            f[i] = val[it - lis.begin() - 1] + a[i].th;
+            if(f[i] > val[it - lis.begin()])
+            {
+                val[it - lis.begin()] = f[i];
+                *it = a[i].sc;
+                if(it -lis.begin() == lis.size() - 1) mxl = i;
+            }
         }
     }
 
     vector<pair<pair<long long, long long>, long long>> kq;
     kq.push_back(a[mxl]);
     long long kb = mxl;
-    for(long long i = mxl - 1; i >= 0; i--)
+    for(long long i = mxl - 1; i >= 1; i--)
     {
-        if(a[kb].fi >= a[i].sc && f[kb] > f[i])
+        if(a[kb].fi >= a[i].sc && f[kb] - a[kb].th == f[i])
         {
             kq.push_back(a[i]);
             kb = i;
@@ -56,7 +62,7 @@ int main()
 
     reverse(kq.begin(), kq.end());
 
-    cout << kq.size() << endl;
+    cout << f[mxl] << endl;
     for(auto &x : kq) cout << x.fi << " " << x.sc << " " << x.th << endl;
 
     return 0;
