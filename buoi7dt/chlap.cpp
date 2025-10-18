989969f34605b38520eb95f0ee58853b5f0dc7cc
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2025-10-18 16:01:57 +0700

    Solution Problem chinhhoplap

---
+++ buoi7dt/chlap.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, k; 
+const int M = 1e6;
+int x[M];
+
+void exp()
+{
+    for(int i = 1; i <= k; i++)
+    {
+        cout << x[i] << " "; 
+    }
+    cout << endl;
+}
+
+void cs(int j)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        x[j] = i;
+        if(j == k) exp();
+        else cs(j + 1);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    cin >> n >> k;
+
+    cs(1);
+
+    return 0;
+}
