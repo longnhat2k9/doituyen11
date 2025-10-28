43faf52d77aea1e9fd3c011d0ab4bd3c53822f96
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2025-10-28 14:58:57 +0700

    Solution Problem fair

--- buoi9dt/fair.cpp
+++ buoi9dt/fair.cpp
@@ -1,16 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long long n, s; 
+long long s; 
 long long a[6000], f[6000], sum = 0, cnt = 0;
 
-void cs(int j)
+void cs(int j, int n, vector<long long>&t)
 {
     for(long long i = 0; i <= 1; i++)
     {
         f[j] = i;
         if(i == 1) sum += a[j];
-        if(sum < s && j < n) cs(j + 1);
+        if(sum < s) 
+        {
+            if(j == n) t.push_back(sum);
+            if(j < n) cs(j + 1, n, t);
+        }
         else if(sum == s) cnt++;
         if(i == 1) sum -= a[j];
     }
@@ -19,11 +23,20 @@ void cs(int j)
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
 
+    long long n;
     memset(f, 0, sizeof(f));
     cin >> n >> s;
     for(long long i = 0; i < n; i++) cin >> a[i];
-    cs(0);
-    cout << cnt;
+    vector<long long>t1, t2;
+    cs(0, n / 2 - 1, t1);
+    cs(n / 2, n - 1, t2);
+
+    sort(t1.begin(), t1.end());
+    for(auto &x : t2)
+    {
+        cnt += upper_bound(t1.begin(), t1.end(), s - x) - lower_bound(t1.begin(), t1.end(), s - x);
+    }
+    cout << cnt % 123456789 << endl;
 
     return 0;
 }
